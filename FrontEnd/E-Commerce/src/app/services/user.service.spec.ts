604b2a542a6c43c3078a51544bd4e19d22799bf6
Harsha <seemakurthiharsha4u@gmail.com>
2024-04-22 06:07:55 +0530

    registerandLogin component

--- FrontEnd/E-Commerce/src/app/services/user.service.spec.ts
+++ FrontEnd/E-Commerce/src/app/services/user.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserServiceTsService } from './user-service.ts.service';
+import { UserService } from './user.service';
 
-describe('UserServiceTsService', () => {
-  let service: UserServiceTsService;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserServiceTsService);
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
